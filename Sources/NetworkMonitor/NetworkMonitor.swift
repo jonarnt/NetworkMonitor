f5fea20ea0cc8262f0a8d4cc0f60acb60e5b538d
Jon Arntzen <jonarnt@mac.com>
2022-11-27 11:49:50 +0000

    Added some comments

--- Sources/NetworkMonitor/NetworkMonitor.swift
+++ Sources/NetworkMonitor/NetworkMonitor.swift
@@ -1,6 +1,22 @@
-public struct NetworkMonitor {
-    public private(set) var text = "Hello, World!"
+// From Rebeloper Youtube
 
-    public init() {
+import SwiftUI
+import Network
+
+public class NetworkMonitor: ObservableObject {
+    private let networkMonitor = NWPathMonitor()
+    private let workerQueue = DispatchQueue(label: "Monitor")
+    public var isConnected: Bool = false
+    
+    public init(){
+        networkMonitor.pathUpdateHandler = { path in
+            self.isConnected = path.status == .satisfied
+            Task {
+                await MainActor.run {
+                    self.objectWillChange.send()
+                }
+            }
+        }
+        networkMonitor.start(queue: workerQueue)
     }
 }
